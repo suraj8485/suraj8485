d57a19821cff4d73ccabee66d30e86d974daec99
suraj sharma <surajsuraj8964007665@gmail.com>
2024-10-06 18:40:48 +0530

    Add files via upload

---
+++ day9-code block.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+float a,b;
+printf("enter the value");
+scanf("%f%f",&a,&b);
+printf("a+b=%f\n",a+b);
+printf("a-b=%f\n",a-b);
+printf("a*b=%f\n",a*b);
+printf("a/b=%f\n",a/b);
+}
